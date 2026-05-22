6b12ba873719be56b2ad5fdf5d76b74d180ba648
Pablo Hernandez <phernandezm07@gmail.com>
2026-05-22 12:35:52 -0500

    chore: initialize settings with defaults instead of nil

--- lua/nvimpack-selector/init.lua
+++ lua/nvimpack-selector/init.lua
@@ -1,11 +1,4 @@
-local M = {
-  settings = nil
-}
-
-local win_data = {
-  buf = -1,
-  win = -1
-}
+---@alias selector_column "name" | "rev" | "src"
 
 local defaults = {
   ui = {
@@ -19,15 +12,37 @@ local defaults = {
       { " " },
       { "[d] delete", "DiagnosticFloatingWarn" },
     }
+  },
+  columns = {
+    --- Columns to display and their order.
+    ---@type selector_column[]
+    order = { "name", "rev", "src" },
+
+    --- Width of each column based on the order they are displayed.
+    --- Negative value make the column fills the available space.
+    --- Non specified values takes -1 automatically.
+    --- If you want to hide a column that is in order, give it a width of 0.
+    --- Note that this values may affect the size of the windows if the sum is bigger than the provided in the ui options.
+    ---@type integer[]
+    width = { 20, 7 },
   }
 }
 
+local M = {
+  settings = defaults
+}
+
+local win_data = {
+  buf = -1,
+  win = -1
+}
+
 M.setup = function(opts)
-  M.settings = vim.tbl_deep_extend("force", defaults, opts)
+  M.settings = vim.tbl_deep_extend("force", M.settings, opts)
 end
 
 M.open = function()
-  win_data.buf, win_data.win = require("nvimpack-selector.ui").open_float(M.settings and M.settings.ui or defaults.ui)
+  win_data.buf, win_data.win = require("nvimpack-selector.ui").open_float(M.settings.ui)
 
   vim.api.nvim_create_autocmd("WinClosed", {
     pattern = tostring(win_data.win),
