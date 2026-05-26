4e222a0f3de984ef63dedb01767b8648351e59ee
Pablo Hernandez <phernandezm07@gmail.com>
2026-05-26 12:37:34 -0500

    feat: add list utils Simple and generic functions for adding elements to a list.
    - Add an element between other elements of a list.
    - Add an element at the start of a list.
    - Add an element at the end of a list.

---
+++ lua/nvimpack-selector/utils/lists.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+---Add an element between the elements of a list.
+---@param list any[]
+---@param element any
+---@return any[]
+M.intersperse = function(list, element)
+  assert(type(list) == "table", "`list` must be a list")
+
+  if #list == 0 then
+    return {}
+  end
+
+  local new = {}
+
+  for i, v in ipairs(list) do
+    if i > 1 then
+      table.insert(new, element)
+    end
+
+    table.insert(new, v)
+  end
+
+  return new
+end
+
+--- Surround a list with an element.
+--- Add the element to the start and the end of the list.
+--- @param list any[]
+--- @param elstart any
+--- @param elend any
+--- @return any[]
+M.surround = function(list, elstart, elend)
+  assert(type(list) == "table", "`list` must be a list")
+
+  local new = {}
+
+  if elstart ~= nil then
+    table.insert(new, elstart)
+  end
+
+  for _, v in ipairs(list) do
+    table.insert(new, v)
+  end
+
+  if elend ~= nil then
+    table.insert(new, elend)
+  end
+
+  return new
+end
+
+return M
